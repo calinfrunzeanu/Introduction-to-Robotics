40edba94118644ef339e8a53421c54b5449b1e2f
Calin Frunzeanu <147422798+calinfrunzeanu@users.noreply.github.com>
2024-11-07 17:55:09 +0200

    Update main.cpp

--- folder/project 2/code/PROJECT 2/src/main.cpp
+++ folder/project 2/code/PROJECT 2/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-// configuratia pinilor pentru led rgb si butoane
+// configuratia pinilor pentru LED RGB si butoane
 const int ledRGB_R = 6;
 const int ledRGB_G = 5;
 const int ledRGB_B = 4;
@@ -16,10 +16,10 @@ const long gameDuration = 30000; // durata totala a jocului in milisecunde
 long currentInterval = 5000;     // interval implicit pentru dificultate
 int difficultyLevel = 0;         // 0: easy, 1: medium, 2: hard
 unsigned long lastDebounceTimeStartStop = 0; // debounce pentru butonul start/stop
-unsigned long lastDebounceTimeLevel = 0;     // debounce pentru butonul de dificultate
-const unsigned long debounceDelay = 200;
+unsigned long lastDebounceTimeLevel = 0; // debounce pentru butonul de dificultate
+const unsigned long debounceDelay = 200; // delay mai mare pentru debounce
 
-// dictionar de cuvinte in romana
+// dictionar de cuvinte
 const char* dictionar[] = {
     "mar", "banana", "cirese", "pruna", "zmeura", "strugure", "morcov",
     "cartof", "patrunjel", "ardei", "castravete", "rosie", "dovleac",
@@ -27,13 +27,7 @@ const char* dictionar[] = {
     "gutui", "usturoi", "ceapa", "praz", "telina", "pastarnac", "cires"
 };
 const int dictSize = sizeof(dictionar) / sizeof(dictionar[0]);
-String currentWord;
-
-// temporizari pentru blink si feedback
-unsigned long blinkStartMillis = 0;
-unsigned long feedbackStartMillis = 0;
-int blinkCount = 0;
-bool ledOn = true;
+String currentWord; // cuvantul curent care trebuie introdus de utilizator
 
 // prototipuri de functii
 void setLEDColor(bool red, bool green, bool blue);
@@ -56,48 +50,42 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(btnStartStop), startStopISR, FALLING);
     attachInterrupt(digitalPinToInterrupt(btnLevel), difficultyISR, FALLING);
 
-    setLEDColor(true, true, true);
+    setLEDColor(true, true, true); // LED alb in starea initiala
 
     Serial.begin(9600);
     randomSeed(analogRead(0));
 
-    Serial.println("selectati dificultatea si apasati start pentru a incepe.");
+    Serial.println("Selectati dificultatea si apasati start pentru a incepe.");
 }
 
 void loop() {
-    unsigned long currentMillis = millis();
-
     if (inGame) {
+        unsigned long currentMillis = millis();
+        
+        // verifica durata totala a jocului
         if (currentMillis - startMillis >= gameDuration) {
             stopGame();
         } else {
+            // verifica inputul de la utilizator numai cand asteptam input
             if (awaitingInput && Serial.available() > 0) {
                 checkInput();
             }
         }
     }
-
-    blinkLED(3);
 }
 
 void setLEDColor(bool red, bool green, bool blue) {
-    digitalWrite(ledRGB_R, red ? HIGH : LOW);
-    digitalWrite(ledRGB_G, green ? HIGH : LOW);
-    digitalWrite(ledRGB_B, blue ? HIGH : LOW);
+    digitalWrite(ledRGB_R, red ? HIGH : LOW);   // HIGH aprinde culoarea rosie
+    digitalWrite(ledRGB_G, green ? HIGH : LOW); // HIGH aprinde culoarea verde
+    digitalWrite(ledRGB_B, blue ? HIGH : LOW);  // HIGH aprinde culoarea albastra
 }
 
 void blinkLED(int times) {
-    unsigned long currentMillis = millis();
-    if (blinkCount < times * 2) {
-        if (currentMillis - blinkStartMillis >= 500) {
-            blinkStartMillis = currentMillis;
-            ledOn = !ledOn;
-            setLEDColor(ledOn, ledOn, ledOn);
-            blinkCount++;
-        }
-    } else if (blinkCount >= times * 2) {
-        setLEDColor(false, false, false);
-        blinkCount = 0;
+    for (int i = 0; i < times; i++) {
+        setLEDColor(true, true, true); // alb
+        delay(500);
+        setLEDColor(false, false, false); // stins
+        delay(500);
     }
 }
 
@@ -105,68 +93,71 @@ void startGame() {
     inGame = true;
     awaitingInput = true;
     nrCuvinte = 0;
-    startMillis = millis();
-    blinkCount = 0;
-
-    Serial.println("jocul a inceput! introduceti cuvintele afisate:");
-    displayWord();
+    startMillis = millis(); // start cronometrul general pentru durata jocului
+    blinkLED(3); // numaratoare inversa
+    setLEDColor(false, false, false); // stinge LED-ul la inceputul jocului
+    Serial.println("Jocul a inceput! Introduceti cuvintele afisate:");
+    displayWord(); // afiseaza primul cuvant
 }
 
 void stopGame() {
     inGame = false;
     awaitingInput = false;
-    setLEDColor(true, true, true);
-    Serial.print("jocul s-a terminat. numar cuvinte corecte: ");
+    setLEDColor(true, true, true); // LED alb la sfarsitul jocului
+    Serial.print("Jocul s-a terminat. Numar cuvinte corecte: ");
     Serial.println(nrCuvinte);
-    Serial.println("selectati dificultatea si apasati start pentru a incepe.");
+    Serial.println("Selectati dificultatea si apasati start pentru a incepe.");
 }
 
 void changeDifficulty() {
     difficultyLevel = (difficultyLevel + 1) % 3;
     switch (difficultyLevel) {
         case 0:
-            currentInterval = 7000;
-            Serial.println("mod easy activat!");
+            currentInterval = 7000; // timp crescut la 7 secunde pentru modul easy
+            Serial.println("Mod easy activat!");
             break;
         case 1:
-            currentInterval = 3000;
-            Serial.println("mod medium activat!");
+            currentInterval = 3000; // 3 secunde pentru modul medium
+            Serial.println("Mod medium activat!");
             break;
         case 2:
-            currentInterval = 1000;
-            Serial.println("mod hard activat!");
+            currentInterval = 1000; // 1 secundă pentru modul hard
+            Serial.println("Mod hard activat!");
             break;
     }
 }
 
+
 void displayWord() {
     int randIndex = random(0, dictSize);
-    currentWord = String(dictionar[randIndex]);
-    Serial.print("introduceti cuvantul: ");
+    currentWord = String(dictionar[randIndex]); // salveaza cuvantul curent
+    Serial.print("Introduceti cuvantul: ");
     Serial.println(currentWord);
-    awaitingInput = true;
+    awaitingInput = true; // asteapta inputul utilizatorului
 }
 
 void checkInput() {
     String input = Serial.readStringUntil('\n');
-    input.trim();
+    input.trim(); // elimina spatiile albe de la inceput si sfarsit
     
     if (input.length() == 0) {
-        Serial.println("input gol detectat - astept input valid.");
-        return;
+        Serial.println("Input gol detectat - astept input valid.");
+        return; // iese din functie daca nu a primit input valid
     }
 
-    if (input.equals(currentWord)) {
+    if (input.equals(currentWord)) { // compara cu cuvantul salvat
         nrCuvinte++;
-        setLEDColor(false, true, false);
-        Serial.println("corect!");
+        setLEDColor(false, true, false); // LED verde pentru raspuns corect
+        Serial.println("Corect!");
     } else {
-        setLEDColor(true, false, false);
-        Serial.println("gresit!");
+        setLEDColor(true, false, false); // LED rosu pentru raspuns gresit
+        Serial.println("Gresit!");
     }
     
-    setLEDColor(false, false, false);
-    displayWord();
+    delay(500);
+    setLEDColor(false, false, false); // stingem LED-ul
+    awaitingInput = false; // seteaza inputul ca fiind primit
+    displayWord(); // afiseaza urmatorul cuvant
 }
 
 void startStopISR() {
